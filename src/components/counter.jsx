b4682be375b5e165cd7e854f6ee23104b0ca2b45
ritik <ritikbhatt020@gmail.com>
2024-01-06 22:48:21 +0530

    Completed props and added Navbar

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,66 +2,71 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   // state is basically an object that includes any data that this component needs
-  state = {
-    count: 0,
-    imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   imageUrl: "https://picsum.photos/200",
+  //   tags: ["tag1", "tag2", "tag3"],
+  // };
+  // I will be making this a controlled component by removing local state.. It receives data using only props 
 
   style = {
     fontSize: "50px",
     fontWeight: "bold",
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags</p>;
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map((tag) => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 
-//   constructor(){
-//     super()
-//     this.handleIncrement = this.handleIncrement.bind(this)
-//   }
+  //   constructor(){
+  //     super()
+  //     this.handleIncrement = this.handleIncrement.bind(this)
+  //   }
 
-  handleIncrement = product => {
-    // console.log("Increment Clicked", this);
-    // this.state.count++   by doing this, count is incrementing but it is not known to react so msg is not passed to Real DOM
-    console.log(product)
-    this.setState( { count: this.state.count + 1 } )
-  }
+  // handleIncrement = () => {
+  //   // console.log("Increment Clicked", this);
+  //   // this.state.count++   by doing this, count is incrementing but it is not known to react so msg is not passed to Real DOM
+  //   // console.log(product);
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   //we have a simple react component which when rendered returns this
   render() {
+    // console.log("props", this.props); // every react component has a property called props which includes all the attributes from counters component 
+
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt=""></img> */}
+        {/* {this.props.children} */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter.id)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        {this.renderTags()}
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+        {/* {this.renderTags()} */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
